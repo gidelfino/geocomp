bce1252b0f3f74b4a72ee055a1ab9b829f24fbdf
gidelfino <giovana.delfino@gmail.com>
2018-11-05 16:10:04 -0200

    ymonotono arrumado

--- geocomp/triangulacao/ymonotono.py
+++ geocomp/triangulacao/ymonotono.py
@@ -3,38 +3,68 @@
 
 from geocomp.common.polygon import Polygon
 from geocomp.common import control
+from geocomp.common.segment import Segment
 from geocomp.common.guiprim import *
 
-def compare(p1, p2):
-	return p1.y < p2.y
+def is_adj(ia, ib, n):
+	print('Adj ', ia, ' ', ib)
+	if (ia == ib + 1 or ia == ib - 1):
+		return True
+	if (ia == 0 and ib == n - 1):
+		return True
+	if (ib == 0 and ia == n - 1):
+		return True
+	return False
+
+def mostra_diagonal(v, w):
+    print(v.x, ' ', v.y, ' ', w.x, ' ', w.y)
+    Segment(v, w).hilight()
 
 def YMonotono (l):
 	"Algoritmo Triangulacao de Poligono Y-Monotono para uma lista l de pontos"
-	p = [0, 2, 1, 3]
-	st = [0] * (len(l) + 1)
+	pol = Polygon(l)
+	pol.plot()
+	p = []
+	n = len(l)
+	for i in range(0, n):
+		p.append(i)
+	p.sort(key=lambda x:l[x].y, reverse=True)
+	for i in range (0, len(l)):
+		# print(l[p[i]].x, ' ', l[p[i]].y)
+		print(p[i], ' ')
+	st = [0] * (n + 1)
 	sz = 0
-	for i in range (0, len(p)):
-		if len(st) == 0:
-			st.append(p[i])
-			sz = sz + 1
+	for i in range(0, n):
+		if sz <= 1:
+			st[sz] = p[i]
+			sz += 1
 		else:
-			lst = st[sz - 1]
-			#vizinho da base da pilha
-			if ((lst > 0 and l[lst - 1] == l[p[i]]) or (lst < len(l) - 1 and l[lst + 1] == l[p[i]])):
-				while(sz > 1 and left(l[p[i]], l[p[sz - 2]], l[p[sz - 1]])):
-					sz = sz - 1
-					print('Diagonal ', l[p[i]].__repr__(), ' ', l[p[sz]].__repr__())
+			b = st[sz - 1]
+			f = st[0]
+			print('back ', b, ' front ', f)
+			if (is_adj(p[i], b, n) and not is_adj(p[i], f, n)):
+				while (sz > 1 and left(l[p[i]], l[st[sz - 1]], l[st[0]])):
+					sz -= 1
+					mostra_diagonal(l[p[i]], l[st[sz - 1]])
 				st[sz] = p[i]
-				sz = sz + 1
-			else:
+				sz += 1
+			elif (is_adj(p[i], f, n) and not is_adj(p[i], b, n)):
+				aux = b
 				while (sz > 1):
-					sz = sz - 1
-					print('Diagonal ', l[p[i]].__repr__(), ' ', l[p[sz]].__repr__())
-				sz = sz - 1
-				st[sz] = lst
-				sz = sz + 1
+					mostra_diagonal(l[p[i]], l[st[sz - 1]])
+					sz -= 1
+				sz -= 1
+				st[sz] = aux
+				sz += 1
 				st[sz] = p[i]
-				sz = sz + 1
+				sz += 1
+			else:
+				sz -= 1
+				while (sz > 2):
+					sz = sz - 1
+					mostra_diagonal(l[p[i]], l[st[sz - 1]])
 
-	tr = Polygon (l)
-	return tr
+
+
+
+			
